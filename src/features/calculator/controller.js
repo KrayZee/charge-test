b4fbbce363ddc76fc3154daa2d8e7c25dd0089ae
Kirill Sudarushkin <kirill.sudarushkin@firstlinesoftware.com>
2015-09-18 15:08:13 +0300

    Finished with form and summary info layout

--- src/features/calculator/controller.js
+++ src/features/calculator/controller.js
@@ -1,5 +1,16 @@
 export default class CalculatorController {
     constructor() {
+        this.countries = [
+            { name: 'United Kingdom', code: 'uk' }
+        ];
+
+        this.chargeTruck = {
+            name: 'Charge 6t 32 kWh'
+        };
+
+        this.analogueTruck = {
+            name: 'Isuzu NPR 6t'
+        };
     }
 }
 
